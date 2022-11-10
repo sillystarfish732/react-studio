591aee398ff3b6522573eaf4bacd693b8ff25687
Lucia Tian <114535761+sillystarfish732@users.noreply.github.com>
2022-11-09 19:09:03 -0500

    cart and bakery items showing up

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { useState } from "react";
 import bakeryData from "./assets/bakery-data.json";
+import BakeryItem from "./components/BakeryItem";
 
 /* ####### DO NOT TOUCH -- this makes the image URLs work ####### */
 bakeryData.forEach((item) => {
@@ -11,17 +12,28 @@ bakeryData.forEach((item) => {
 function App() {
   // TODO: use useState to create a state variable to hold the state of the cart
   /* add your cart state code here */
-
+  const [cart, setCart] = useState([])
+  const [totalPrice, setPrice] = useState(0)
+  const addToCart = (itemName, itemPrice) => {
+    setCart ([...cart, itemName])
+    setPrice(totalPrice + itemPrice);
+   }
   return (
     <div className="App">
-      <h1>My Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
+      <h1>My Bakery!!!</h1> {/* TODO: personalize your bakery (if you want) */}
 
       {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
-        <p>Bakery Item {index}</p> // replace with BakeryItem component
+        //<p>Bakery Item {index}</p> // replace with BakeryItem component
+        <BakeryItem nameProp ={item} prop2={index} priceProp={item} desciptionProp={item}
+        imageProp={item.image} updateCart={addToCart} />
       ))}
 
       <div>
         <h2>Cart</h2>
+        {cart.map((itemName, itemPrice)=> {
+          return <p>{itemName}</p>
+        })}
+        {totalPrice}
         {/* TODO: render a list of items in the cart */}
       </div>
     </div>
--- src/components/BakeryItem.js
+++ src/components/BakeryItem.js
@@ -1,1 +1,19 @@
 // TODO: create a component that displays a single bakery item
+
+export default function BakeryItem({nameProp, priceProp, desciptionProp, prop2, 
+    imageProp, updateCart}){
+
+    return(
+        <div>
+            <img style={{width: '20vw', marginTop:'3rem'}} src={imageProp} 
+            alt={nameProp.name}/>
+            <br></br>
+            {prop2} {nameProp.name} {priceProp.price} {desciptionProp.description}
+            <br></br>
+            <button onClick={()=> updateCart(nameProp.name, priceProp.price)}>Add to Cart</button>
+        </div>
+
+
+       
+    )
+}
